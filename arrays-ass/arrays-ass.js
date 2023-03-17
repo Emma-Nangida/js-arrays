4a50ac686d6e25736548b99bd5d12c9e495e44fe
Emma-Nangida <emmanangida@gmail.com>
2023-03-17 19:46:57 +0300

    first commit

---
+++ arrays-ass/arrays-ass.js
@@ -0,0 +1,29 @@
+//Find the last element of the following arrays.
+//arr1 = [3,7,34,90,12]
+//arr2 = [true, "green", "where",12,56]
+let array=[3,7,34,90,]
+
+//Write a JS program that will join the following array elements into a string
+//myPets = ["Cow", "Python", "Snake", "Dog"];
+
+
+//Write a JS script to sort the following array items
+//var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+
+//Write a program to remove duplicates from the following array.
+//Console the array without duplicates, and console another array that only contains the duplicates
+//var arr = ["apple", "mango", "apple",
+            //"orange", "mango", "mango"];
+
+//Write a JS script to search for the following word in the array.
+//4
+//If the word is present, console it else console "the search word was not found"
+//let arr5 = ["the", "way", "x", 4, 23];
+
+//Write a JS script to sort the following string
+//let word = "lufituaeb"
+
+
+
+
+
