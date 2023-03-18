df332a91339f6e822fc429537f37014f870bb0a0
Emma-Nangida <emmanangida@gmail.com>
2023-03-18 12:13:07 +0300

    changes

--- arrays-ass/arrays-ass.js
+++ arrays-ass/arrays-ass.js
@@ -1,19 +1,33 @@
 //Find the last element of the following arrays.
 //arr1 = [3,7,34,90,12]
 //arr2 = [true, "green", "where",12,56]
-let array=[3,7,34,90,]
+
+let array1=[3,7,34,90,12];
+let lastElement=array1.slice(-1);
+console.log(lastElement);
+
+array2=[true,"green","where",12,56];
+let lastelement=array2.slice(-1);
+console.log(lastelement);
+
 
 //Write a JS program that will join the following array elements into a string
 //myPets = ["Cow", "Python", "Snake", "Dog"];
+let array=["Cow","Python","Snake","Dog"];
+console.log(array.join(''));
 
 
 //Write a JS script to sort the following array items
 //var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+var arr3=[-5,9,5,3,2,-3,6,8,4,1];
+arr3.sort();
+console.log(arr3);
 
 //Write a program to remove duplicates from the following array.
 //Console the array without duplicates, and console another array that only contains the duplicates
 //var arr = ["apple", "mango", "apple",
             //"orange", "mango", "mango"];
+   
 
 //Write a JS script to search for the following word in the array.
 //4
@@ -21,7 +35,11 @@ let array=[3,7,34,90,]
 //let arr5 = ["the", "way", "x", 4, 23];
 
 //Write a JS script to sort the following string
-//let word = "lufituaeb"
+//let word = "lufituaeble"
+let word="lufituable";
+   let z=word.split("");
+   z.sort();
+   console.log(z);
 
 
 
